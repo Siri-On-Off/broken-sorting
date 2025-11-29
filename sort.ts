26a71d66a46b6f914cb621d1eea164f58dcbf2a6
Patrick Bucher <patrick.bucher@mailbox.org>
2025-11-29 09:39:34 +0100

    use swap function in insertion sort

--- sort.ts
+++ sort.ts
@@ -19,9 +19,7 @@ export function insertionSort(numbers: Array<number>): Array<number> {
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < i; j++) {
       if (xs[i] < xs[j]) {
-        const swap = xs[i];
-        xs[i] = xs[j];
-        xs[j] = swap;
+        swap(xs, i, j);
       }
     }
   }
