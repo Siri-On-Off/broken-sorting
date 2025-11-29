b884608835e9bd2747d1d36d990ff407ba648252
Patrick Bucher <patrick.bucher@mailbox.org>
2025-11-29 08:11:07 +0100

    initial commit: scaffolding

---
+++ main.ts
@@ -0,0 +1,23 @@
+import { bubbleSort, insertionSort, selectionSort } from "./sort.ts";
+
+function generateRandomNumbers(
+  lower: number,
+  upper: number,
+  n: number
+): Array<number> {
+  const numbers: Array<number> = [];
+  if (lower >= upper || n < 1) {
+    return numbers;
+  }
+  for (let i = 0; i < n; i++) {
+    numbers.push(Math.floor(Math.random() * (upper - lower + 1)) + lower);
+  }
+  return numbers;
+}
+
+const numbers = generateRandomNumbers(1, 100, 10);
+console.log(`Original:       ${numbers.join(" ")}`);
+console.log(`Built-In Sort:  ${numbers.toSorted().join(" ")}`);
+console.log(`Bubble Sort:    ${bubbleSort(numbers).join(" ")}`);
+console.log(`Insertion Sort: ${insertionSort(numbers).join(" ")}`);
+console.log(`Selection Sort: ${selectionSort(numbers).join(" ")}`);
---
+++ sort.ts
@@ -0,0 +1,11 @@
+export function bubbleSort(numbers: Array<number>): Array<number> {
+  return numbers;
+}
+
+export function insertionSort(numbers: Array<number>): Array<number> {
+  return numbers;
+}
+
+export function selectionSort(numbers: Array<number>): Array<number> {
+  return numbers;
+}
