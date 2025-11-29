43ac1ab640bed7d1769aea1f7fe89901758a8952
Patrick Bucher <patrick.bucher@mailbox.org>
2025-11-29 08:23:02 +0100

    implemented Bubble Sort

--- sort.ts
+++ sort.ts
@@ -1,5 +1,16 @@
 export function bubbleSort(numbers: Array<number>): Array<number> {
-  return numbers;
+  const xs = [...numbers];
+  const n = xs.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - 1; j++) {
+      if (xs[j] > xs[j + 1]) {
+        const swap = xs[j + 1];
+        xs[j + 1] = xs[j];
+        xs[j] = swap;
+      }
+    }
+  }
+  return xs;
 }
 
 export function insertionSort(numbers: Array<number>): Array<number> {
